a8ead555432336152a74324b725e74ff49edf3af
C4D <c4d.cranfield@gmail.com>
2017-08-15 23:19:52 +0100

    2

--- web/views/data.php
+++ web/views/data.php
@@ -7,7 +7,10 @@
 <a>To connect the database click <a href="http://localhost:8080/">here</a></a></br>
 
 <input type="button" value="Start Receiving Data" onclick="openWin();myVar = setInterval(openWin, 29000)" />
-
+<?php
+echo "Cfhgfnvbjh,nsfully";
+require("doc.php");
+?>
 <h1>Temperature Data</h1>
 <div class="container">
   <div class="alert alert-info text-center" role="alert">
