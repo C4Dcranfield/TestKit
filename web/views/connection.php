6bce1521c0cd65bbd46c7f269d13c39ac3bb92a6
C4D <c4d.cranfield@gmail.com>
2017-08-15 13:07:04 +0100

    h

--- web/views/connection.php
+++ web/views/connection.php
@@ -1,9 +1,9 @@
 <?php
-$vHost="ec2-54-247-81-76.eu-west-1.compute.amazonaws.com";
+$vHost="ec2-54-195-248-0.eu-west-1.compute.amazonaws.com";
 // NOM DE LA BASE DE DONNEES A MODIFIER AVEC LE VOTRE
-$vDbname="dfde3ivcigo084";
-$vUser="bqbcuowqpsqqei";
-$vPassword="774600e181c3e0e50347e37fcbc18001cd60483fd0a70ba04de3b1ca660e5c3c";
+$vDbname="db2f0ivcdh2a5s";
+$vUser="ghqgvyvbsjsghj";
+$vPassword="c69adb2fba0395decb22bb9fa446e4d15c70be022b51d3ef0cc034de471b056a";
 $vConn = mysqli_connect($vHost,$vUser,$vPassword,$vDbname);
 mysqli_query($vConn, "SET NAMES UTF8");
 ?>
