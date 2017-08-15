8ed24e5e8f057304c9e94d593761e8f992920355
C4D <c4d.cranfield@gmail.com>
2017-08-15 22:36:19 +0100

    f

--- web/views/data.php
+++ web/views/data.php
@@ -26,55 +26,6 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully";
 
-/*include('connection.php');
-
-$query = "DROP TABLE Temperature";
-$result = $db->query($query);
-
-$query = "CREATE TABLE Temperature
-		(
-		Value_T int,
-		kit_ID int,
-		);";
-$result = $db->query($query);
-
-$query ="INSERT INTO `db2f0ivcdh2a5s`.`temperature` (`Value_T`, `kit_ID`) VALUES ('25', '1');";
-$result = $db->query($query);
-
-$query = "SELECT Value_T, kit_ID FROM temperature;";
-$result = $db->query($query);
-
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "<tr>";
-    echo "<td>" . $row["Value_T"] . "</td>";
-    echo "<td>" . $row["kit_ID"] . "</td>";	
-    echo "</tr>";
-}
-$result->closeCursor();
-	
-$vSql ="DROP TABLE Temperature";
-$vQuery=mysqli_query($vConn,$vSql);
-
-$vSql ="CREATE TABLE Temperature
-		(
-		Value_T int,
-		kit_ID int,
-		);";
-$vQuery=mysqli_query($vConn,$vSql);
-
-$vSq2 ="INSERT INTO `db2f0ivcdh2a5s`.`temperature` (`Value_T`, `kit_ID`) VALUES ('25', '1');";
-$vQuery2=mysqli_query($vConn,$vSq2);
-
-$vSq3="SELECT Value_T, kit_ID FROM temperature;";
-$vQuery3=mysqli_query($vConn,$vSq3);
-
-while ($vResult = mysqli_fetch_array($vQuery3)) 
-			{
-echo "<td>$vResult[ID_T]</td>";
-echo "<td>$vResult[Value_T]</td>";
-			};
-
-*/
 
 ?>
 </div>
