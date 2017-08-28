60b24c281c4ad280ba4f4be67341d6cad37f915c
C4D <c4d.cranfield@gmail.com>
2017-08-28 16:16:16 +0100

    Revert "c"
    This reverts commit cf9173a84b02a2962d4cae30d9bdcc132b2cec31.

--- web/views/data.php
+++ web/views/data.php
@@ -15,8 +15,26 @@ echo "Cfhgfnvbjh,nsfully";
 <div class="container">
   <div class="alert alert-info text-center" role="alert">
 <?php
+<<<<<<< HEAD
 echo "Cfhgfnvbjh,nsfully";
 require("doc.php");
+=======
+
+$servername = "ec2-54-195-248-0.eu-west-1.compute.amazonaws.com";
+$username = "ghqgvyvbsjsghj";
+$password = "c69adb2fba0395decb22bb9fa446e4d15c70be022b51d3ef0cc034de471b056a";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+
+>>>>>>> parent of cf9173a... c
 ?>
 </div>
 </div>
